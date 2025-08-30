b2f734f125a0af817a6b3f9f4123ef94ed34b261
2200032265kl <2200032265@kluniversity.in>
2025-08-30 17:15:56 +0530

    db models created

---
+++ backend/src/config/db.js
@@ -0,0 +1,16 @@
+
+import mongoose from "mongoose";
+import { ENV } from "./env.js"
+
+export const connectDB = async () => {
+    try
+    {
+        await mongoose.connect(ENV.MONGO_URI)
+        console.log("connected to DB succesfully ✅")
+    }
+    catch(error)
+    {
+        console.log("error connecting to MONGODB ❌");
+        process.exit(1)
+    }
+}
